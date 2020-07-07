78c2b74cdbbef6adb7ca6dd77b5519c102dac26b
Manuel Marcano <mmarcano@technisys.com>
2020-07-06 21:49:06 -0300

    reading countries

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <fstream>
 #include <cstring>
+#include <stdlib.h>
 
 using namespace std;
 const int MAX_COUNTRY = 20;
-const string COUNTRY_FILENAME = "Paises.txt";
-const string COUNTRY_STATISTICS_FILENAME = "ParteDiario.txt";
+const string COUNTRY_FILENAME = "/home/manu/UTN/TP1_BORLAN/Paises.txt";
+const string COUNTRY_STATISTICS_FILENAME = "/home/manu/UTN/TP1_BORLAN/ParteDiario.txt";
 
 struct country {
     char countryName[20];
@@ -22,17 +22,17 @@ struct countryStatistics {
     char countryName[20];
     int day,
         month,
-        infecteds,
-        hisopades,
-        recovereds,
-        deads;
+        infected,
+        hisopade,
+        recovered,
+        deaths;
     void print() const {
         cout << " Pais: " << countryName << " - ";
         cout << " Fecha: "<< day << "/" << month << " - ";
-        cout << " Infectados: "<< infecteds ;
-        cout << " hisopados: "<< hisopades ;
-        cout << " Curados: "<< recovereds ;
-        cout << " Muertes: "<< deads << endl;
+        cout << " Infectados: "<< infected ;
+        cout << " hisopados: "<< hisopade ;
+        cout << " Curados: "<< recovered ;
+        cout << " Muertes: "<< deaths << endl;
     }
 };
 
@@ -40,20 +40,43 @@ void genCountryFile(){
     FILE *countryFile;
     int i;
     struct country countries[MAX_COUNTRY];
-    string countries_names[MAX_COUNTRY] = {"Venezuela","Bolivia","Peru","Argentina","Uruguay","Paraguay","Colombia","España","Paris","Italia","Portugal","Mexico","Panama","China","Japon","Rusia","Canada","Brasil","Chile","Inglaterra"};
-    string continet[MAX_COUNTRY] = {"America","America","America","America","America","America","America","Europa","Europa","Europa","Europa","America","America","Asia","Asia","Asia","America","America","America","Europa"};
+    string countries_names[MAX_COUNTRY] = {"Venezuela","Bolivia","Peru","Argentina","Uruguay","Paraguay","Colombia","Espaï¿½a","Paris","Italia","Portugal","Mexico","Panama","China","Japon","Rusia","Canada","Brasil","Chile","Inglaterra"};
+    string continent[MAX_COUNTRY] = {"America","America","America","America","America","America","America","Europa","Europa","Europa","Europa","America","America","Asia","Asia","Asia","America","America","America","Europa"};
     countryFile = fopen(COUNTRY_FILENAME.c_str(), "wb");
     if (countryFile != NULL){
         for (i = 0; i< MAX_COUNTRY; i++) {
             memset(&countries[i], 0, sizeof(country));
             strcpy(countries[i].countryName, countries_names[i].c_str());
-            strcpy(countries[i].continent, continet[i].c_str());
-            countries[i].people = 3000;
+            strcpy(countries[i].continent, continent[i].c_str());
+            countries[i].people = rand() % 3000000 + 5000000;
             fwrite(&countries[i], sizeof(struct country), 1, countryFile);
          }
     }
     fclose(countryFile);
+}
 
+void genDailyCountryFile(){
+    FILE *countryFile;
+    int i;
+    struct countryStatistics countries[MAX_COUNTRY];
+    string countries_names[MAX_COUNTRY] = {"Venezuela","Bolivia","Peru","Argentina","Uruguay","Paraguay","Colombia","Espaï¿½a","Paris","Italia","Portugal","Mexico","Panama","China","Japon","Rusia","Canada","Brasil","Chile","Inglaterra"};
+    countryFile = fopen(COUNTRY_STATISTICS_FILENAME.c_str(), "wb");
+    if (countryFile != NULL){
+        for (i = 0; i< MAX_COUNTRY; i++) {
+            memset(&countries[i], 0, sizeof(country));
+
+            strcpy(countries[i].countryName, countries_names[i].c_str());
+            countries[i].day = rand() % 31 + 1;
+            countries[i].month = rand() % 12 + 1;
+            countries[i].infected = rand() % 3000000 + 5000000;
+            countries[i].hisopade = rand() % 3000000 + 5000000;
+            countries[i].recovered = rand() % 3000000 + 5000000;
+            countries[i].deaths = rand() % 3000000 + 5000000;
+            fwrite(&countries[i], sizeof(struct countryStatistics), 1, countryFile);
+            countries[i].print();
+        }
+    }
+    fclose(countryFile);
 }
 
 bool readCountryFile(FILE *countryFile, country countries[]){
@@ -62,9 +85,25 @@ bool readCountryFile(FILE *countryFile, country countries[]){
     countryFile = fopen(COUNTRY_FILENAME.c_str(), "r+b");
     if (countryFile != NULL){
         couldRead = true;
-        while (!feof(countryFile)){
+        while (!feof(countryFile) && i < MAX_COUNTRY){
           memset(&countries[i], 0, sizeof(country));
           fread(&countries[i], sizeof(struct country), 1, countryFile);
+          i++;
+        }
+    }
+    fclose(countryFile);
+    return couldRead;
+}
+
+bool readDailyCountry(FILE *countryFile, countryStatistics countries[]){
+    int i = 0;
+    bool couldRead = false;
+    countryFile = fopen(COUNTRY_STATISTICS_FILENAME.c_str(), "r+b");
+    if (countryFile != NULL){
+        couldRead = true;
+        while (!feof(countryFile) && i < MAX_COUNTRY){
+          memset(&countries[i], 0, sizeof(countryStatistics));
+          fread(&countries[i], sizeof(struct countryStatistics), 1, countryFile);
           i++;
         }
     }
@@ -78,17 +117,32 @@ void openFiles(FILE **countryFile, FILE **statisticsFile){
     *statisticsFile = fopen("Paises.txt", "r+b");
 }
 
+void closeFiles(FILE **countryFile, FILE **statisticsFile){
+    fclose(*countryFile);
+    fclose(*statisticsFile);
+}
+
 int main() {
     FILE *countryFile, *statisticsFile;
-    struct country countries[MAX_COUNTRY];
+    country countries[MAX_COUNTRY];
+    countryStatistics countriesPart[MAX_COUNTRY];
     int i;
     cout << "Generando Archivos..." << endl;
     genCountryFile();
+    genDailyCountryFile();
+
+    cout << "Leyendo Archivos..." << endl;
     openFiles(&countryFile, &statisticsFile);
     readCountryFile(countryFile, countries);
-    cout << "---- PRINTING ----" << endl;
+    readDailyCountry(statisticsFile, countriesPart);
+
+
+
+
+    cout << "Printing" << endl;
     for ( i=0; i< MAX_COUNTRY; i++ ){
-        countries[i].print();
+        countriesPart[i].print();
     }
+    closeFiles(&countryFile, &statisticsFile);
     return 0;
 }
